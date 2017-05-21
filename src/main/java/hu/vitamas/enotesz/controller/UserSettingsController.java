eab975374b49c8a9e4654eb953d8bb32b295f549
Vincze Tamás Zoltán <v.tamas.zoltan@gmail.com>
2017-05-21 14:46:19 +0200

    1.0

--- src/main/java/hu/vitamas/enotesz/controller/UserSettingsController.java
+++ src/main/java/hu/vitamas/enotesz/controller/UserSettingsController.java
@@ -17,15 +17,24 @@
 package hu.vitamas.enotesz.controller;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import hu.vitamas.enotesz.dao.UsersDao;
+import hu.vitamas.enotesz.json.SimpleApiResponse;
 import hu.vitamas.enotesz.model.Users;
 import hu.vitamas.enotesz.util.Auth;
+import hu.vitamas.enotesz.util.Core;
+import hu.vitamas.enotesz.util.MapBuilder;
 import hu.vitamas.enotesz.view.Alerts;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -63,14 +72,13 @@ public class UserSettingsController implements Initializable {
 	public void initialize(URL url, ResourceBundle rb) {
 
 		Users user = Auth.getUser();
-		if(user != null)
-		{
+		if (user != null) {
 			Boolean daily = user.getDailyReport() == 1;
 			dailyreport.setSelected(daily);
 		}
-		
-		saveBtn.setOnMouseClicked(this::save);
 
+		saveBtn.setOnMouseClicked(this::save);
+		deleteAccountBtn.setOnMouseClicked(this::deleteAccount);
 	}
 
 	private void save(MouseEvent ev) {
@@ -78,21 +86,65 @@ public class UserSettingsController implements Initializable {
 		Boolean needDailyReport = dailyreport.isSelected();
 
 		Users user = Auth.getUser();
-		if(user != null)
-		{
+		if (user != null) {
 			Integer dailyflag = needDailyReport ? 1 : 0;
-			
+
 			user.setDailyReport(dailyflag);
 			UsersDao dao = new UsersDao();
 			dao.update(user);
-			
+
 			Alerts.success("Sikeres módosításra került!").show();
-		}
-		else {
+		} else {
 			Alerts.error("Hiba történt!").show();
 		}
 
 	}
 
+	private void deleteAccount(MouseEvent ev) {
+		SimpleApiResponse response = sendDeleteRequest();
+		if (response != null) {
+			if (response.getSuccess()) {
+				Core.openWebsite("https://enotesz.vitamas.hu/goodbye");
+				Platform.exit();
+			} else {
+				Alerts.warning(response.getErrors().replaceAll("<br>", " ")).show();
+				logger.error("api delete acc - Status: " + response.getStatus());
+			}
+		} else {
+			Alerts.warning("Hiba történt, kérlek próbáld újra később!");
+		}
+	}
 
+	private SimpleApiResponse sendDeleteRequest() {
+
+		String apiKey = Core.getApiKey();
+		String apiLink = Core.getApiBaseUrl();
+		String pass = deletePasswordField.getText();
+		Integer id = Auth.getUserID();
+
+		if (apiKey == null || apiLink == null)
+			return null;
+
+		HashMap<String, String> params = MapBuilder.<String, String>newHashMap()
+				.set("userid", id.toString())
+				.set("password", pass)
+				.set("apiKey", apiKey)
+				.build();
+
+		try {
+			String response = Core.createApi()
+					.setMethod("POST")
+					.setUrl(apiLink + "user/deleteAccount")
+					.setParams(params)
+					.sendRequest();
+			if (response == null)
+				return null;
+			Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+			return gson.fromJson(response, SimpleApiResponse.class);
+		} catch (Exception e) {
+			logger.error("api or gson error", e);
+		}
+
+		return null;
+	}
 }
